cd6f058a373a9cbaccb8e645d57f37f11b51c008
Simon Dirmeier <simon.dirmeier@web.de>
2020-04-27 14:05:45 +0200

    Add D3 for vis

--- app/view.js
+++ app/view.js
@@ -1,39 +1,63 @@
 "use strict";
 
-define(function () {
+define(function (require) {
+  const d3 = require("libs/d3");
+
   class View {
     constructor(model) {
-      this._width = 650;
-      this._height = 650;
-      this._app = this._getElement('#app');
+      this._width = 600;
+      this._height = 600;
+      this._app = this._getApp();
       this._model = model;
       this._board = this._model.board;
       this._initBoard();
     }
 
-    _getElement(selector) {
-      return document.querySelector(selector);
-    }
-
-    _createElement(tag) {
-      const svgns = "http://www.w3.org/2000/svg";
-      return document.createElementNS(svgns, tag);
+    _getApp() {
+      return d3.select("#app")
+        .attr("align", "center")
+        .style("width", this._width)
+        .style("height", this._height);
     }
 
     _svg() {
-      const svg = this._createElement("svg");
-      this._app.appendChild(svg);
-      svg.setAttribute("width", this._width - 5);
-      svg.setAttribute("height", 10);
-      return svg;
+      return this._app
+        .append("svg")
+        .attr("width", this._width )
+        .attr("height", this._height / this._board.length);
     }
 
-    _rect(row) {
-      const rect = this._createElement("rect");
-      rect.setAttribute("width", (this._width - 5) / this._board[row].length);
-      rect.setAttribute("height", 20);
-      rect.setAttribute("x", ((this._width - 15) / this._board[row].length) * row);
-      return rect;
+    _g(svg) {
+      return svg.append("g");
+    }
+
+    _rect(el, row, col) {
+      return el
+        .append("rect")
+        .attr("width", (this._width - 5) / (this._board[row].length))
+        .attr("height", this._height / (this._board.length))
+        .attr("x", ((this._width) / this._board[row].length) * col);
+      //attr("points", "0,50 150,50 150,150 50,150");
+    }
+
+    _text(el, rect, row, col) {
+      const w = ((this._width / this._board[row].length)) / 8;
+      return el
+        .append("text")
+        .text("(" + row + "," + col + ")")
+        .attr('fill', 'black')
+        .attr("x", parseFloat(rect.attr('x')) + w)
+        .attr("y", rect.attr('y') + 12)
+        .attr('font-size', '8')
+        .attr('fill', 'black');
+    }
+
+    _path(el, rect, row, col) {
+      if (col !== 0)
+        return;
+      return el
+        .append("polyline")
+        .attr("points", rect.attr('x') + ",0 0," + rect.attr("height") );
     }
 
     _initBoard() {
@@ -41,21 +65,12 @@ define(function () {
       for (let i = 0; i < board.length; i++) {
         const svg = this._svg();
         for (let j = 0; j < board[i].length; j++) {
-          const tile = board[i][j].draw();
-          const rect = this._rect(i);
-          svg.append(rect);
+          const g = this._g(svg);
+          const rect = this._rect(g, i, j);
+          const text = this._text(g, rect, i, j);
+          const path = this._path(g, rect, i, j);
         }
       }
-
-      // const rect2 = this._createElement("rect");
-      // rect2.setAttributeNS(null, 'x', '50');
-      // rect2.setAttributeNS(null, 'height', '50');
-      // rect2.setAttributeNS(null, 'width', '50');
-
-      // attach it to the container
-      // svg.appendChild(rect);
-      // svg.appendChild(rect2);
-
     }
 
     _draw() {
