838c7c6e3f2532f7ace962fd52391abc61d4affd
Simon Dirmeier <simon.dirmeier@web.de>
2020-04-27 20:10:11 +0200

    Finish init board

--- app/model/board/tile.js
+++ app/model/board/tile.js
@@ -1,16 +1,33 @@
 "use strict";
 
 define(function () {
+
   class Tile {
     constructor(name, room, i, j, gate) {
       this._name = name;
       this._room = room;
       this._i = i;
       this._j = j;
+      this._direction = {
+        '>': false,
+        '<': false,
+        '^': false,
+        'v': false
+      };
       this._gate = gate;
+      if (this._gate !== null)
+        this._direction[this._gate] = true;
       this._isOccupied = false;
       this._occupant = null;
       this._neighbors = {up: null, down: null, left: null, right: null};
+    }
+
+    get name() {
+      return this._name;
+    }
+
+    get gate() {
+      return this._gate;
     }
 
     get neighbors() {
@@ -34,17 +51,54 @@ define(function () {
       return this._occupant;
     }
 
+    isOtherRoomAndNoGate(neighbor) {
+      return this.isOtherRoom(neighbor) && this.isNoGate(neighbor);
+    }
+
+    isOtherRoomAndGate(neighbor) {
+      return this.isOtherRoom(neighbor) && this.isGate();
+    }
+
+    isOtherRoomAndNoNeighborVerticalGate(neighbor) {
+      return this.isOtherRoom(neighbor)
+         && neighbor.isVerticalGate();
+    }
+
+    isOtherRoom(neighbor) {
+      return neighbor !== null && neighbor.name !== this.name;
+    }
+
+    isGate() {
+      return this.gate !== null;
+    }
+
+    isVerticalGate() {
+      return this.isGateRight() || this.isGateLeft();
+    }
+
+    isGateRight() {
+      return this._direction[">"];
+    }
+
+    isGateLeft() {
+      return this._direction["<"];
+    }
+
+
+    isNoGate(neighbor) {
+      return neighbor !== null && this.gate === null && neighbor.gate === null;
+    }
+
     draw() {
       let draw = "";
       if (this._gate !== null) {
 
       }
-      if (this._neighbors.left === null) {
-        draw += "|";
-      }
+
       if (this._isOccupied && (this._neighbors.down === null ||
         this._neighbors.down._name !== this._name)) {
         draw += "%";
+
       } else if (this._isOccupied && this._gate !== null) {
         draw += "&";
       } else if (this._isOccupied) {
@@ -61,9 +115,8 @@ define(function () {
       } else {
         draw += " ";
       }
-      if (this._neighbors.right === null) {
-        draw += "|";
-      } else if (this._neighbors.right._name !== this._name && this._gate === null &&
+      if (this._neighbors.right._name !== this._name &&
+        this._gate === null &&
         this._neighbors.right._gate === null) {
         draw += "|";
       } else draw += ",";
--- app/view.js
+++ app/view.js
@@ -10,7 +10,10 @@ define(function (require) {
       this._app = this._getApp();
       this._model = model;
       this._board = this._model.board;
+      this._players = this._model.players;
+
       this._initBoard();
+      this._initPlayers();
     }
 
     _getApp() {
@@ -34,10 +37,11 @@ define(function (require) {
     _rect(el, row, col) {
       return el
         .append("rect")
-        .attr("width", (this._width - 1 ) / (this._board[row].length) - 1)
+        .attr("width", (this._width - 1) / (this._board[row].length) - 1)
         .attr("height", (this._height - 1) / (this._board.length) - 1)
-        .attr("x", ((this._width) / this._board[row].length) * col);
-      //attr("points", "0,50 150,50 150,150 50,150");
+        .attr("x", ((this._width) / this._board[row].length) * col)
+        .attr("row", row)
+        .attr("col", col);
     }
 
     _text(el, rect, row, col) {
@@ -52,22 +56,45 @@ define(function (require) {
         .attr('fill', 'black');
     }
 
-    _path(el, rect, row, col) {
+    _path(el, rect, tile) {
+
       const x = parseFloat(rect.attr('x'));
       const w = parseFloat(rect.attr('width'));
       const h = parseFloat(rect.attr('height'));
-      if (col === 0)
+
+      if (tile.neighbors.left === null)
         el.append("polyline").attr(
           "points", "0.5,0 0.5," + h);
-      else if (col === this._board[row].length - 1)
+      else if (tile.neighbors.right === null)
         el.append("polyline").attr(
-          "points", (x + w) + ",0 " +  (x + w) + "," + h);
-      if (row === 0)
+          "points", (x + w) + ",0 " + (x + w) + "," + h);
+
+      if (tile.isOtherRoomAndNoGate(tile.neighbors.right)) {
         el.append("polyline").attr(
-          "points", x + ",0.5 "+ (x + w) + ",0.5");
-      else if (row === this._board.length - 1)
+          "points", (x + w) + ",0 " + (x + w) + "," + h);
+      }
+
+      if (tile.isOtherRoomAndNoGate(tile.neighbors.down)) {
         el.append("polyline").attr(
-          "points", x + ",  " + (h - 1.5)  + " " + (x + w) + "," + (h - 1.5));
+          "points", (x) + ",  " + (h - 2) + " " + (x + w) + "," + (h - 2));
+      }
+
+      if (tile.isOtherRoomAndNoNeighborVerticalGate(tile.neighbors.down)) {
+        el.append("polyline").attr(
+          "points", (x) + ",  " + (h - 2) + " " + (x + w) + "," + (h - 2));
+      }
+
+      if (tile.isGateRight() && tile.isOtherRoom(tile.neighbors.down)) {
+        el.append("polyline").attr(
+          "points", (x) + ",  " + (h - 2) + " " + (x + w) + "," + (h - 2));
+      }
+
+      if (tile.neighbors.up === null)
+        el.append("polyline").attr(
+          "points", x + ",0.5 " + (x + w) + ",0.5");
+      else if (tile.neighbors.down === null)
+        el.append("polyline").attr(
+          "points", x + ",  " + (h - 2) + " " + (x + w) + "," + (h - 2));
     }
 
     _initBoard() {
@@ -75,11 +102,28 @@ define(function (require) {
       for (let i = 0; i < board.length; i++) {
         const svg = this._svg();
         for (let j = 0; j < board[i].length; j++) {
+          const tile = board[i][j];
           const g = this._g(svg);
           const rect = this._rect(g, i, j);
-          const text = this._text(g, rect, i, j);
-          const path = this._path(g, rect, i, j);
+          this._text(g, rect, i, j);
+          this._path(g, rect, tile);
         }
+      }
+    }
+
+    
+
+    _draw() {
+      const board = this._model.board;
+      for (let i = 0; i < board.length; i++) {
+        let draws = "";
+        let holds = "";
+        for (let j = 0; j < board[i].length; j++) {
+          draws += board[i][j].draw();
+          if (board[i][j].occupied)
+            holds += board[i][j].occupant;
+        }
+        console.log(draws + "\t" + holds);
       }
     }
   }
